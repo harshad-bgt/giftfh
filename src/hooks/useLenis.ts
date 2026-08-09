8ebe57c21b48af6dc1c2a76485574fdb8ab4b3ba
Harshad Bhagat <harshadbgt@gmail.com>
2026-08-09 23:52:15 +0530

    perf: Optimize Lenis scroll and remove expensive GPU compositing

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -9,11 +9,11 @@ gsap.registerPlugin(ScrollTrigger);
 export function useLenis() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.5,
+      duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       wheelMultiplier: 1,
-      touchMultiplier: 2,
+      touchMultiplier: 1,
     });
 
     lenis.on('scroll', ScrollTrigger.update);
@@ -21,8 +21,6 @@ export function useLenis() {
     gsap.ticker.add((time) => {
       lenis.raf(time * 1000);
     });
-
-    gsap.ticker.lagSmoothing(0);
 
     return () => {
       gsap.ticker.remove((time) => lenis.raf(time * 1000));
